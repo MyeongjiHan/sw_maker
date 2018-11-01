f677acca44be5735ab62988e75bb0d81bb68e684
MyeongjiHan <41604634+MyeongjiHan@users.noreply.github.com>
2018-11-01 20:30:55 +0900

    스토리보드 네비게이션컨 추가

--- yourThing/yourThing/SecondViewController.swift
+++ yourThing/yourThing/SecondViewController.swift
@@ -10,57 +10,57 @@ import UIKit
 
 class SecondViewController: UIViewController, XMLParserDelegate {
     
-    var xmlParser = XMLParser()
-    var currentElement = ""
-    var placeAddrs = [String]()
-    var placeAddr = ""
-    var resultPlaces = ""
-    
-    @IBOutlet weak var nameLabel: UILabel!
-    
-    func requestInfo() {
-        
-        let url = "http://apis.data.go.kr/1320000/LosPtfundInfoInqireService/getPtLosfundInfoAccToLc?serviceKey=XuU01vYHKB%2BUi3h%2FZXvu5%2BI7BJ5fP%2BB%2FLmrFscEhUDLAJfB2hTCKnu73ZJcpS9kDVtqYxxEAhJ6XB79kQKE4Sg%3D%3D"
-        
-        guard let xmlParser = XMLParser(contentsOf: URL(string: url)!) else {return}
-        
-        xmlParser.delegate = self
-        xmlParser.parse()
-        
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        requestInfo()
-        // Do any additional setup after loading the view.
-        
-        for item in placeAddrs {
-            resultPlaces = resultPlaces + "\n\n" + "\(item)"
-            print("습득장소는 \(item)\n")
-            //print("\(resultPlaces)")
-        }
-        
-        nameLabel.text = resultPlaces
-    }
-    
-    public func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        currentElement = elementName
-        if(elementName == "depPlace") {
-            placeAddr = ""
-        }
-    }
-    
-    public func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        if(elementName == "depPlace") {
-            placeAddrs.append(placeAddr)
-        }
-    }
-    
-    public func parser(_ parser: XMLParser, foundCharacters string: String) {
-        placeAddr = string
-        
-        print("\(string)")
-    }
+//    var xmlParser = XMLParser()
+//    var currentElement = ""
+//    var placeAddrs = [String]()
+//    var placeAddr = ""
+//    var resultPlaces = ""
+//    
+//    @IBOutlet weak var nameLabel: UILabel!
+//    
+//    func requestInfo() {
+//        
+//        let url = "http://apis.data.go.kr/1320000/LosPtfundInfoInqireService/getPtLosfundInfoAccToLc?serviceKey=XuU01vYHKB%2BUi3h%2FZXvu5%2BI7BJ5fP%2BB%2FLmrFscEhUDLAJfB2hTCKnu73ZJcpS9kDVtqYxxEAhJ6XB79kQKE4Sg%3D%3D"
+//        
+//        guard let xmlParser = XMLParser(contentsOf: URL(string: url)!) else {return}
+//        
+//        xmlParser.delegate = self
+//        xmlParser.parse()
+//        
+//    }
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        requestInfo()
+//        // Do any additional setup after loading the view.
+//        
+//        for item in placeAddrs {
+//            resultPlaces = resultPlaces + "\n\n" + "\(item)"
+//            print("습득장소는 \(item)\n")
+//            //print("\(resultPlaces)")
+//        }
+//        
+//        nameLabel.text = resultPlaces
+//    }
+//    
+//    public func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+//        currentElement = elementName
+//        if(elementName == "depPlace") {
+//            placeAddr = ""
+//        }
+//    }
+//    
+//    public func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+//        if(elementName == "depPlace") {
+//            placeAddrs.append(placeAddr)
+//        }
+//    }
+//    
+//    public func parser(_ parser: XMLParser, foundCharacters string: String) {
+//        placeAddr = string
+//        
+//        print("\(string)")
+//    }
     
     /*
      // MARK: - Navigation
