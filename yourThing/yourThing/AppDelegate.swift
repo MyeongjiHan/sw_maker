2c4fed351581b76fa33e5a238da9d716d9161377
CAU <cau@CAUui-iMac.local>
2018-11-01 20:35:30 +0900

    google map

--- yourThing/yourThing/AppDelegate.swift
+++ yourThing/yourThing/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey("AIzaSyAec1tsaaMACnbDCZsmxdjpkjthl0mYr5k")
         GMSServices.provideAPIKey("AIzaSyAec1tsaaMACnbDCZsmxdjpkjthl0mYr5k")
         return true
     }
