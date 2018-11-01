37c83dab33cd4c1a4240c7b5853ae899c19a7378
MyeongjiHan <41604634+MyeongjiHan@users.noreply.github.com>
2018-11-01 20:25:21 +0900

    스토리보드 정비

---
+++ yourThing/yourThing/TransportationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TransportationViewController.swift
+//  yourThing
+//
+//  Created by CAU on 01/11/2018.
+//  Copyright © 2018 CAU. All rights reserved.
+//
+
+import UIKit
+
+class TransportationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
